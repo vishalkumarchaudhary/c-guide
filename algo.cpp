31f23f020108b0aa36e6d80ceb145670caca9352
mabrin <111501030@smail.iitpkd.ac.in>
2018-08-18 22:32:04 +0530

    todo updates

--- algo.cpp
+++ algo.cpp
@@ -2,3 +2,4 @@
 	1) count the array element into count array 
 	2) sum the adjacent of count to get the index of element in sorted array
 	3) parse through original array and place that element into new array with indexing as count says
+
